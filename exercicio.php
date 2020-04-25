ffcd35e06c3cff326e020585c94695a4c62a0812
marcelotakayama <marcelo.takayama00@gmail.com>
2020-04-25 06:21:38 -0300

    Finalizando exercícios sem formatação

--- exercicio.php
+++ exercicio.php
@@ -13,13 +13,13 @@
         <h2>Vizualização do exercício</h2>
     </header>
     <nav class="navegacao">
-        <a href="" class="verde">Sem Formatação</a>
+        <a href=<?= "{$_GET['dir']}/{$_GET['file']}.php" ?> class="verde">Sem Formatação</a>
         <a href="index.php" class="vermelho">Voltar</a>
     </nav>
     <main class="principal">
         <div class="conteudo">
             <?php
-                include('teste/teste.php')
+                include(__DIR__."/{$_GET['dir']}/{$_GET['file']}.php");
             ?>        
         </div>
     </main>
